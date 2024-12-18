65987a55ee5c1ddbceb1f54461acfa3e4d67095f
Ndiyakholwa <mnqanqenindiyakholwa206gmail.com>
2024-12-18 02:05:20 +0200

    imagflip generating images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react"
+
 export default function Main() {
+    const [dataMemes,setDataMemes]= useState([])
+    const [meme,setMeme]=useState({
+        topText:"One does not simply",
+        bottomText:"Walk into Mordor",
+        imgUrl:"http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleBottom(event){
+        setMeme(prevMeme=>({...prevMeme,bottomText:event.target.value}))
+    }
+    function handleTop(event){
+        setMeme(prevMeme=>({...prevMeme,topText:event.target.value}))
+    }
+
+    function handleImg(){
+        setMeme(prevMeme=>({...prevMeme,imgUrl:dataMemes[0][Math.floor(Math.random()*dataMemes[0].length)].url}))
+    }
+
+    useEffect(()=>{
+        fetch('https://api.imgflip.com/get_memes')
+        .then(response=>{
+            if(!response.ok){
+                throw new Error("Failed to fetch memes");
+            }
+            return response.json();
+        }).then(data=>{
+            setDataMemes([data.data.memes])
+        })
+    })
     return (
         <main>
             <div className="form">
@@ -7,6 +38,7 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleTop}
                     />
                 </label>
 
@@ -15,14 +47,15 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleBottom}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={handleImg}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imgUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom" >{meme.bottomText}</span>
             </div>
         </main>
     )
